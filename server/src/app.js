9dc0b1b6e93a1c5c52b854fc5c70748a3f0607a5
Read-Blanc <nwanonezesamuel@gmail.com>
2025-05-07 16:39:45 +0100

    Updated app

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ import commentRoutes from "./routes/comment.js";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:4600", "https://instaclone-self-six.vercel.app"],
+  origin: ["http://localhost:4600", "https://insta-shot-liart.vercel.app/"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
